5c8a0dea5f195908ce199c1d6eeccccacf930a12
JewelryKim <jsjs21good@gmail.com>
2023-01-28 21:28:00 +0900

    settings

---
+++ components/layout.tsx
@@ -0,0 +1,21 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import Footer from "./footer";
+import Navbar from "./navbar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <Head>
+                <title>Jewel Blog</title>
+                <meta name="description" content="Jewel Blog" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+        </>
+    );
+};
+
+
