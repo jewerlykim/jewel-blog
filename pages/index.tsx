dac2c2383f36aa771df469beed59902fead96261
JewelryKim <jsjs21good@gmail.com>
2023-06-14 23:41:24 +0900

    og metadata added

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,17 @@ import HomeContainer from '../components/home/home.container';
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <meta property="og:url" content="https://godjewel.co.kr/" />
+        <meta
+          property="og:image"
+          content="https://github.com/jewerlykim/jewel-blog/assets/75651834/9e699ea1-8a28-401f-9657-f6edcf3b08c9"
+        />
+        <meta property="og:image:width" content="640" />
+        <meta property="og:image:height" content="640" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <title>JewelLog</title>
+      </Head>
       <main className="min-h-screen">
         <HomeContainer />
       </main>
